0bbf5dd88956c6b70aa816ee61aa5cf3f227bf6e
UrbanJuraj <durko11169@gmail.com>
2022-04-13 19:48:57 +0200

    adding router logic for App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+
+import Layout from "./components/layout/Layout";
+import WeatherInformation from "./components/MainScreen/WeatherInformation";
+import Search from "./components/SearchScreen/Search";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<WeatherInformation />} />
+        <Route path="search" element={<Search />} />
+      </Routes>
+    </Layout>
+  );
 }
 
 export default App;
