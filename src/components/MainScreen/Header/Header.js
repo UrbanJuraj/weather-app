4a51af6d8d3eb4c5e1f5a20ee2b0ee45dbfad94a
UrbanJuraj <durko11169@gmail.com>
2022-04-15 14:54:39 +0200

    display city and date from redux

--- src/components/MainScreen/Header/Header.js
+++ src/components/MainScreen/Header/Header.js
@@ -3,13 +3,17 @@ import { Link } from "react-router-dom";
 import { MdPlace } from "react-icons/md";
 
 import styles from "./Header.module.css";
+import { useSelector } from "react-redux";
 
 const Header = (props) => {
+  const city = useSelector((state) => state.city.city);
+  const date = useSelector((state) => state.city.date);
+
   return (
     <header className={styles.header}>
-      <p>Wednesday, 08 Oct 2020 | 4:30PM</p>
+      <p>{date}</p>
       <Link to="/search">
-        Kosice, Slovakia
+        {city}
         <MdPlace />
       </Link>
     </header>
