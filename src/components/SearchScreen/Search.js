b34fc891b1759df52c7a3d3d304f33f961f5f855
UrbanJuraj <durko11169@gmail.com>
2022-04-15 18:42:48 +0200

    create array with cities for display

--- src/components/SearchScreen/Search.js
+++ src/components/SearchScreen/Search.js
@@ -4,20 +4,56 @@ import styles from "./Search.module.css";
 import SearchedCity from "./SearchedCity/SearchedCity";
 
 const Search = (props) => {
+  const cities = [
+    {
+      name: "Bratislava",
+      lat: 48.1482,
+      lon: 17.1067,
+    },
+    {
+      name: "Humenné",
+      lat: 48.9371,
+      lon: 21.9163,
+    },
+    {
+      name: "Koromľa",
+      lat: 48.7445,
+      lon: 22.1814,
+    },
+    {
+      name: "Košice",
+      lat: 48.6667,
+      lon: 21.3333,
+    },
+    {
+      name: "Michalovce",
+      lat: 48.7543,
+      lon: 21.9195,
+    },
+    {
+      name: "Sobrance",
+      lat: 48.7445,
+      lon: 22.1814,
+    },
+  ];
+
+  const citiesJsx = cities.map((city) => (
+    <SearchedCity
+      key={city.name}
+      city={city.name}
+      lat={city.lat}
+      lon={city.lon}
+      temperature="39"
+    />
+  ));
+
   return (
     <main className={styles.search}>
       <h2>Location</h2>
 
       <Form />
 
-      <div className={styles.cities}>
-        <SearchedCity city="Bratislava" temperature="35" />
-        <SearchedCity city="Humenne" temperature="35" />
-        <SearchedCity city="Koromla" temperature="35" />
-        <SearchedCity city="Kosice" temperature="35" />
-        <SearchedCity city="Michalovce" temperature="35" />
-        <SearchedCity city="Sobrance" temperature="35" />
-      </div>
+      <div className={styles.cities}>{citiesJsx}</div>
     </main>
   );
 };
