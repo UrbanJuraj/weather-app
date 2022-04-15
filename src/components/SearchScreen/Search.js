1b921533965c9363ad58b73661eb7ff980fcf4be
UrbanJuraj <durko11169@gmail.com>
2022-04-15 21:03:01 +0200

    adding logic for search cities

--- src/components/SearchScreen/Search.js
+++ src/components/SearchScreen/Search.js
@@ -1,43 +1,30 @@
-import Form from "./Form/Form";
-import styles from "./Search.module.css";
+import { useRef, useState } from "react";
+
+import { MdPlace } from "react-icons/md";
 
 import SearchedCity from "./SearchedCity/SearchedCity";
 
-const Search = (props) => {
-  const cities = [
-    {
-      name: "Bratislava",
-      lat: 48.1482,
-      lon: 17.1067,
-    },
-    {
-      name: "Humenné",
-      lat: 48.9371,
-      lon: 21.9163,
-    },
-    {
-      name: "Koromľa",
-      lat: 48.7445,
-      lon: 22.1814,
-    },
-    {
-      name: "Košice",
-      lat: 48.6667,
-      lon: 21.3333,
-    },
-    {
-      name: "Michalovce",
-      lat: 48.7543,
-      lon: 21.9195,
-    },
-    {
-      name: "Sobrance",
-      lat: 48.7445,
-      lon: 22.1814,
-    },
-  ];
+import styles from "./Search.module.css";
 
-  const citiesJsx = cities.map((city) => (
+const Search = () => {
+  const [filteredCities, setFilteredCities] = useState([]);
+  const [isSearched, setIsSearched] = useState(false);
+  const userInputRef = useRef();
+
+  const onChangeHandler = () => {
+    setFilteredCities(
+      cities.filter((city) =>
+        city.name
+          .toLowerCase()
+          .startsWith(userInputRef.current.value.toLowerCase())
+      )
+    );
+    setIsSearched(true);
+  };
+
+  if (filteredCities.length === 0 && !isSearched) setFilteredCities(cities);
+
+  const citiesJsx = filteredCities.map((city) => (
     <SearchedCity
       key={city.name}
       city={city.name}
@@ -51,7 +38,16 @@ const Search = (props) => {
     <main className={styles.search}>
       <h2>Location</h2>
 
-      <Form />
+      <form>
+        <div>
+          <input
+            placeholder="Search city ..."
+            onChange={onChangeHandler}
+            ref={userInputRef}
+          />
+          <MdPlace className={styles.icon} />
+        </div>
+      </form>
 
       <div className={styles.cities}>{citiesJsx}</div>
     </main>
@@ -59,3 +55,36 @@ const Search = (props) => {
 };
 
 export default Search;
+
+const cities = [
+  {
+    name: "Bratislava",
+    lat: 48.1482,
+    lon: 17.1067,
+  },
+  {
+    name: "Humenné",
+    lat: 48.9371,
+    lon: 21.9163,
+  },
+  {
+    name: "Koromľa",
+    lat: 48.7445,
+    lon: 22.1814,
+  },
+  {
+    name: "Košice",
+    lat: 48.6667,
+    lon: 21.3333,
+  },
+  {
+    name: "Michalovce",
+    lat: 48.7543,
+    lon: 21.9195,
+  },
+  {
+    name: "Sobrance",
+    lat: 48.7445,
+    lon: 22.1814,
+  },
+];
