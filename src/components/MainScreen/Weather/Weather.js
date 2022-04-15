c1c19df96e08438ec503efd04dee98c5253c1d7d
UrbanJuraj <durko11169@gmail.com>
2022-04-15 13:15:17 +0200

    changing displayed weather description

--- src/components/MainScreen/Weather/Weather.js
+++ src/components/MainScreen/Weather/Weather.js
@@ -14,24 +14,30 @@ import styles from "./Weather.module.css";
 const Weather = () => {
   const weather = useSelector((state) => state.city.weather);
 
+  let description = weather;
   let icon = <WiFog className={styles.icon} />;
 
   if (weather.includes("Drizzle") || weather.includes("Rain")) {
     icon = <WiRain className={styles.icon} />;
+    description = "Rainy";
   } else if (weather.includes("Clouds")) {
     icon = <WiDayCloudy className={styles.icon} />;
+    description = "Cloudy";
   } else if (weather.includes("Clear")) {
     icon = <WiDaySunny className={styles.icon} />;
+    description = "Sunny";
   } else if (weather.includes("Snow")) {
     icon = <WiSnow className={styles.icon} />;
+    description = "Snowy";
   } else if (weather.includes("Thunderstorm")) {
     icon = <WiThunderstorm className={styles.icon} />;
+    description = "Storm";
   }
 
   return (
     <div className={styles.weather}>
       {icon}
-      <p>{weather}</p>
+      <p>{description}</p>
     </div>
   );
 };
