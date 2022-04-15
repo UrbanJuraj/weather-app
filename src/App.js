1c3c9d1c96a8e247eecdb48532c39c8c7786d3e7
UrbanJuraj <durko11169@gmail.com>
2022-04-15 21:49:50 +0200

    preparing App.js for using

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { Route, Routes } from "react-router-dom";
+
+import { fetchCityWeather } from "./store/city-actions";
 
 import Layout from "./components/layout/Layout";
 import MainScreen from "./components/MainScreen/MainScreen";
-import Search from "./components/SearchScreen/Search";
+import SearchScreen from "./components/SearchScreen/SearchScreen";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCityWeather(48.6667, 21.3333));
+  }, [dispatch]);
+
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<MainScreen />} />
-        <Route path="search" element={<Search />} />
+        <Route path="search" element={<SearchScreen />} />
       </Routes>
     </Layout>
   );
