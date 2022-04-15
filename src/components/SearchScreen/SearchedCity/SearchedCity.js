cc02382257aca391f6a7093248c5c7a95e195d5f
UrbanJuraj <durko11169@gmail.com>
2022-04-15 18:40:22 +0200

    adding onClickHandler for fetching data

--- src/components/SearchScreen/SearchedCity/SearchedCity.js
+++ src/components/SearchScreen/SearchedCity/SearchedCity.js
@@ -1,8 +1,21 @@
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+import { fetchCityWeather } from "../../../store/city-actions";
+
 import styles from "./SearchedCity.module.css";
 
 const SearchedCity = (props) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const onClickHandler = () => {
+    dispatch(fetchCityWeather(props.lat, props.lon));
+    navigate("/");
+  };
+
   return (
-    <div className={styles.city}>
+    <div className={styles.city} onClick={onClickHandler}>
       <p className={styles["city-name"]}>{props.city}</p>
       <p className={styles.degree}>{props.temperature}&deg;C</p>
     </div>
