238299a62a372ff3f014f90a559229a669347dc7
UrbanJuraj <durko11169@gmail.com>
2022-04-15 12:57:30 +0200

    display weather from redux

--- src/components/MainScreen/Weather/Weather.js
+++ src/components/MainScreen/Weather/Weather.js
@@ -1,12 +1,37 @@
-import { WiDayCloudy } from "react-icons/wi";
+import { useSelector } from "react-redux";
+
+import {
+  WiDayCloudy,
+  WiThunderstorm,
+  WiRain,
+  WiSnow,
+  WiFog,
+  WiDaySunny,
+} from "react-icons/wi";
 
 import styles from "./Weather.module.css";
 
-const Weather = (props) => {
+const Weather = () => {
+  const weather = useSelector((state) => state.city.weather);
+
+  let icon = <WiFog className={styles.icon} />;
+
+  if (weather.includes("Drizzle") || weather.includes("Rain")) {
+    icon = <WiRain className={styles.icon} />;
+  } else if (weather.includes("Clouds")) {
+    icon = <WiDayCloudy className={styles.icon} />;
+  } else if (weather.includes("Clear")) {
+    icon = <WiDaySunny className={styles.icon} />;
+  } else if (weather.includes("Snow")) {
+    icon = <WiSnow className={styles.icon} />;
+  } else if (weather.includes("Thunderstorm")) {
+    icon = <WiThunderstorm className={styles.icon} />;
+  }
+
   return (
     <div className={styles.weather}>
-      <WiDayCloudy className={styles.sunny} />
-      <p>Sunny</p>
+      {icon}
+      <p>{weather}</p>
     </div>
   );
 };
